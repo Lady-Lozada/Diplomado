a59f78fb8227b2abaddb962c4df5f7a632217dfd
Lady-Lozada <lady124ylp@hotmail.com>
2022-01-20 22:08:26 -0500

    Se crea configuracion swagger

--- helpme/src/main/java/com/developers/helpme/HelpmeApplication.java
+++ helpme/src/main/java/com/developers/helpme/HelpmeApplication.java
@@ -1,9 +1,11 @@
 package com.developers.helpme;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class HelpmeApplication {
 
 	public static void main(String[] args) {
